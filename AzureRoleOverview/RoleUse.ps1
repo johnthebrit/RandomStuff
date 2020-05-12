051fa2e2245e74ec8bb47bd1d7c6a13f984468d5
John Savill <john@savilltech.com>
2020-05-12 10:14:19 -0500

    Role Overview content

---
+++ AzureRoleOverview/RoleUse.ps1
@@ -0,0 +1,12 @@
+Get-AzRoleDefinition | FT Name, IsCustom
+Get-AzRoleDefinition 'Network Peering' | convertto-json  # then write to file to change ID to null, change scope and details
+New-AzRoleDefinition -InputFile "networkpeerrole.json"
+New-AzRoleDefinition -InputFile "VMReadandRunExtrole.json"
+
+<#
+https://docs.microsoft.com/en-us/azure/role-based-access-control/custom-roles
+"AssignableScopes": [
+    "/subscriptions/{subscriptionId1}",
+    "/subscriptions/{subscriptionId2}",
+    "/providers/Microsoft.Management/managementGroups/{groupId1}"
+#>
