5db02c58489ce6fba6bbfac75f91890e2a57ef1e
John Savill <john@savilltech.com>
2021-05-30 09:52:50 -0500

    Debug Demo file

---
+++ PowerShellStuff/DebugDemo.ps1
@@ -0,0 +1,48 @@
+function Write-OutRPSOption {
+
+    $RPSChoice = Get-Random -Minimum 1 -Maximum 9
+    Write-Verbose "Random number was $RPSChoice"
+    switch ($RPSChoice) {
+        {$PSItem -ge 1 -and $PSItem -le 3} { write-output "**   ROCK   **" }
+        {$PSItem -ge 4 -and $PSItem -le 6} { write-output "**   PAPER  **" }
+        {$PSItem -ge 7 -and $PSItem -le 9} { write-output "** SCISSORS **" }
+    }
+}
+
+function Start-RPSGame {
+    param (
+        $NoOfRounds
+    )
+
+    $Round = 1
+
+    while($Round -le $NoOfRounds)
+    {
+        for ($RPSCounterDisp = 1; $RPSCounterDisp -le 3; $RPSCounterDisp++) {
+            switch ($RPSCounterDisp) {
+                1 { Write-Output "Rock" }
+                2 { Write-Output "Paper" }
+                3 { Write-Output "Scissors" }
+            }
+            Start-Sleep -Milliseconds 500
+        }
+
+        Write-OutRPSOption
+
+        $Round++
+        Write-Output "`n`n"
+        Start-Sleep -Seconds 2
+    }
+
+
+}
+
+#Main Execution Block
+Write-Output "Three rounds of rock, paper, scissors!"
+Start-Sleep -Seconds 1
+
+Write-Output ""
+
+Start-RPSGame(3)
+
+Write-Output "`nThanks for playing"
