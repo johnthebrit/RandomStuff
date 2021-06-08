65f3812bca4645dae695bf37b937e68432a4cafa
John Savill <john@savilltech.com>
2021-06-08 07:19:27 -0500

    Only show disks for a single region

--- PowerShellStuff/AzDiskList.ps1
+++ PowerShellStuff/AzDiskList.ps1
@@ -1,16 +1,18 @@
-$subID = 'SubID'
+$subID = Get-Secret -Name DevSubID -AsPlainText #if using secret store! Look at PSSsecretManagement.ps1
+#$subID = 'SubID'
 $token = Get-AzAccessToken
 $authHeader = @{
     'Content-Type'='application/json'
     'Authorization'='Bearer ' + $token.Token
 }
-$r = Invoke-WebRequest -Uri "https://management.azure.com/subscriptions/$subID/providers/Microsoft.Compute/skus?api-version=2019-04-01" -Method Get -Headers $authHeader
+$UriString = "https://management.azure.com/subscriptions/$subID/providers/Microsoft.Compute/skus?api-version=2019-04-01&$filter=location eq 'eastus2'"
+$r = Invoke-WebRequest -Uri $UriString -Method Get -Headers $authHeader
 
 $rarray = $r.Content | ConvertFrom-Json # | ConvertTo-Json #Make it pretty
 
 foreach ($item in $rarray.value) {
     write-debug $item
-    if($item.resourceType -eq 'disks')
+    if($item.resourceType -eq 'disks' -and $item.locations -eq 'eastus')
     {
         $MaxSize = 0
         foreach ($attr in $item.capabilities) {
