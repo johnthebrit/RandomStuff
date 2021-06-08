57c7ede69749455f477e0fc311c89af5c6d75039
John Savill <john@savilltech.com>
2021-06-08 07:41:51 -0500

    Changed the disk list to have location in filter instead of logic reducing data fetched

--- PowerShellStuff/AzDiskList.ps1
+++ PowerShellStuff/AzDiskList.ps1
@@ -5,14 +5,15 @@ $authHeader = @{
     'Content-Type'='application/json'
     'Authorization'='Bearer ' + $token.Token
 }
-$UriString = "https://management.azure.com/subscriptions/$subID/providers/Microsoft.Compute/skus?api-version=2019-04-01&$filter=location eq 'eastus2'"
+
+$UriString = "https://management.azure.com/subscriptions/$subID/providers/Microsoft.Compute/skus?api-version=2019-04-01&`$filter=location eq 'eastus2'"
 $r = Invoke-WebRequest -Uri $UriString -Method Get -Headers $authHeader
 
 $rarray = $r.Content | ConvertFrom-Json # | ConvertTo-Json #Make it pretty
 
 foreach ($item in $rarray.value) {
     write-debug $item
-    if($item.resourceType -eq 'disks' -and $item.locations -eq 'eastus')
+    if($item.resourceType -eq 'disks')
     {
         $MaxSize = 0
         foreach ($attr in $item.capabilities) {
