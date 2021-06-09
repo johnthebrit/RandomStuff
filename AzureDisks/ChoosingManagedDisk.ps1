89a55a6d15e6b7f398ead43c1e63e234bf5e1e04
John Savill <john@savilltech.com>
2021-06-09 08:57:19 -0500

    Managed Disk Deep Dive Artifacts

---
+++ AzureDisks/ChoosingManagedDisk.ps1
@@ -0,0 +1,12 @@
+#Perf Test Command
+c:\tools\diskspd -t2 -o32 -b4k -r4k -w0 -d10 -Sh -D -L -c1G e:\test\IO.dat
+
+#Look at all disks
+Get-AzDisk | Select-Object -Property Name, ResourceGroupName,DiskSizeGB,@{Name = 'DiskType'; Expression = {$_.sku.name}}
+
+#Getting a SAS to a managed disk to see really is a storage account behind it!
+$diskSas = Grant-AzDiskAccess -ResourceGroupName 'RG-INFRA-SCUS' -DiskName 'premdisklonely' -DurationInSecond 60 -Access 'Read'
+
+#Change an ultra disk IOPS dynamically, i.e. the VM can be running and connected
+$DiskUpdateconfig = New-AzDiskUpdateConfig -DiskIOPSReadWrite $iops
+Update-AzDisk -ResourceGroupName $RGName -DiskName $SiskName -DiskUpdate $DiskUpdateconfig
