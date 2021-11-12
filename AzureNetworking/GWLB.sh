b204c086432fd284cba5cedd341b5a1dae5014c6
John Savill <john@savilltech.com>
2021-11-12 15:39:30 -0600

    GWLB config

--- AzureNetworking/GWLB.sh
+++ AzureNetworking/GWLB.sh
@@ -8,10 +8,10 @@ sudo apt upgrade
 sudo chmod 777 /etc/sysctl.conf
 echo"net.ipv4.ip_forward = 1">/etc/sysctl.conf
 sudo sysctl -p /etc/sysctl.conf
-sudo iptables -t nat -A POSTROUTING -d 10.0.0.0/8 -j ACCEPT
-sudo iptables -t nat -A POSTROUTING -d 172.16.0.0/12 -j ACCEPT
-sudo iptables -t nat -A POSTROUTING -d 192.168.0.0/16 -j ACCEPT
-sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+#sudo iptables -t nat -A POSTROUTING -d 10.0.0.0/8 -j ACCEPT
+#sudo iptables -t nat -A POSTROUTING -d 172.16.0.0/12 -j ACCEPT
+#sudo iptables -t nat -A POSTROUTING -d 192.168.0.0/16 -j ACCEPT
+#sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 sudo iptables -A FORWARD -j ACCEPT
 
 #disable firewall
@@ -24,8 +24,8 @@ client_external_vni=801
 client_external_port=10801
 nva_lb_ip=10.200.0.5
 
-#MTU of 1600
-sudo ifconfig eth0 mtu 1600
+#MTU of 4000
+sudo ifconfig eth0 mtu 4000
 
 # Internal tunnel
 sudo ip link add vxlan${client_internal_vni} type vxlan id ${client_internal_vni} remote ${nva_lb_ip} dstport ${client_internal_port} nolearning
@@ -52,3 +52,36 @@ ifconfig vxlan${client_internal_vni}
 ip -d link show vxlan${client_internal_vni}
 ip -d link show vxlan${client_external_vni}
 route -n
+
+
+#To make this autoload
+#Create a service file
+sudo vim /etc/systemd/system/nvanetwork.service
+
+[Unit]
+Description=vni service
+
+[Service]
+Type=simple
+ExecStart=/bin/bash /usr/local/bin/nvavnisetup.sh
+
+[Install]
+WantedBy=multi-user.target
+:wq
+
+#Create the file that actually sets up the VNIs
+sudo vim /usr/local/bin/nvavnisetup.sh
+<lines 20-40>
+:wq
+
+#Make usable as a service
+sudo chmod 744 /usr/local/bin/nvavnisetup.sh
+
+sudo chmod 664 /etc/systemd/system/nvanetwork.service
+sudo systemctl daemon-reload
+sudo systemctl enable nvanetwork.service
+
+#To test
+sudo systemctl --type service
+sudo systemctl start nvanetwork.service
+sudo systemctl daemon-reload
