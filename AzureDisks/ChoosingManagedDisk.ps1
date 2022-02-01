e000ed9a545f8e4fa00ccac631207ebcb4ecd32e
John Savill <john@savilltech.com>
2022-02-01 08:33:37 -0600

    PPG and disk

--- AzureDisks/ChoosingManagedDisk.ps1
+++ AzureDisks/ChoosingManagedDisk.ps1
@@ -5,7 +5,9 @@ c:\tools\diskspd -t2 -o32 -b4k -r4k -w0 -d10 -Sh -D -L -c1G e:\test\IO.dat
 Get-AzDisk | Select-Object -Property Name, ResourceGroupName,DiskSizeGB,@{Name = 'DiskType'; Expression = {$_.sku.name}}
 
 #Getting a SAS to a managed disk to see really is a storage account behind it!
-$diskSas = Grant-AzDiskAccess -ResourceGroupName 'RG-INFRA-SCUS' -DiskName 'premdisklonely' -DurationInSecond 60 -Access 'Read'
+$diskSas = Grant-AzDiskAccess -ResourceGroupName 'RG-LiveResize' -DiskName 'disk1' -DurationInSecond 60 -Access 'Read'
+$diskSas
+Revoke-AzDiskAccess -ResourceGroupName 'RG-LiveResize' -DiskName 'disk1'
 
 #Change an ultra disk IOPS dynamically, i.e. the VM can be running and connected
 $DiskUpdateconfig = New-AzDiskUpdateConfig -DiskIOPSReadWrite $iops
