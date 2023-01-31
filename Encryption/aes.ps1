24bb09ed8438bb37fb7a6d0e3e7dae672bc4c577
John Savill <john@savilltech.com>
2023-01-31 07:02:07 -0600

    encryption whiteboard

--- Encryption/aes.ps1
+++ Encryption/aes.ps1
@@ -6,6 +6,8 @@ P6
 1600 1600
 255#>
 #HexEdit to remove these lines
+
 openssl enc -aes-128-ecb -nosalt -pass pass:"onboardtoazure" -in body.ppm -out body.aes
 openssl enc -aes-128-cbc -nosalt -pass pass:"onboardtoazure" -in body.ppm -out body.aescbc
+
 #HexEdit to add back in the 3 lines at the start
