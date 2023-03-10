59a69d600d70989cd8d43f4940c342e12b3cd55c
John Savill <john@savilltech.com>
2023-03-10 08:18:09 -0600

    new subtitle format from divinci handled with b

--- YouTube/formatBookmarks.ps1
+++ YouTube/formatBookmarks.ps1
@@ -1,4 +1,4 @@
 #This is taken from the RegEx\RegExDemos.ps1 example
 $samplestring = Get-Content S:\captures\subtitle.srt -Raw
-$updatedstring = $samplestring -replace '[\s\S]*?(?:\r\n|\r|\n)(\d{2}:\d{2}:\d{2}),\d{1,3} --> \d{2}:\d{2}:\d{2},\d{1,3}(?:\r\n|\r|\n)([\s\S]*?)(?:\r\n|\r|\n)',$('$1 - $2'+"`n")
+$updatedstring = $samplestring -replace '[\s\S]*?(?:\r\n|\r|\n)(\d{2}:\d{2}:\d{2}),\d{1,3} --> \d{2}:\d{2}:\d{2},\d{1,3}(?:\r\n|\r|\n)<b>([\s\S]*?)</b>(?:\r\n|\r|\n)',$('$1 - $2'+"`n")
 $updatedstring | out-file S:\Captures\bookmarks.txt
