6b2080ade0c327bb3f8e6d7fb661208930f7d0ab
John Savill <john@savilltech.com>
2024-05-13 12:40:22 -0700

    Update to checking disk cache for ephemeral

--- AzureVMs/CheckCache.ps1
+++ AzureVMs/CheckCache.ps1
@@ -1,22 +1,27 @@
+Function HasSupportEphemeralOSDisk([object[]] $capability)
+{
+     return $capability | where { $_.Name -eq "EphemeralOSDiskSupported" -and $_.Value -eq "True"}
+}
+
 $location = "southcentralus"
 
 $VMCacheInfo = @()
 
-$VMSKUs = Get-AzComputeResourceSku -Location $location | Where-Object { $_.ResourceType -eq "virtualMachines" } | Where-Object { $null -eq $_.Restrictions.ReasonCode }
+$VMSKUs = Get-AzComputeResourceSku -Location $location | Where-Object { $_.ResourceType -eq "virtualMachines" -and (HasSupportEphemeralOSDisk $_.Capabilities) -ne $null }
 
 foreach ($SKU in $VMSKUs)
 {
-    if (($Sku.Capabilities | Where-Object { $_.Name -eq "EphemeralOSDiskSupported" }).Value -eq $true -and ($Sku.Capabilities | Where-Object { $_.Name -eq "PremiumIO" }).Value -eq $true -and $null -ne ($Sku.Capabilities | Where-Object { $_.Name -eq "CachedDiskBytes" }).Value)
-    {
-        $VMSKU = New-Object PSObject -Property @{
-            Name = $Sku.Name
-            Family = $Sku.Family -replace "standard", ""
-            CachedDiskBytes = (($Sku.Capabilities | Where-Object { $_.Name -eq "CachedDiskBytes" }).Value / 1GB)
-            EphemeralOsDiskSupported = [bool]($Sku.Capabilities | Where-Object { $_.Name -eq "EphemeralOSDiskSupported" }).Value
-            }
-        $VMCacheInfo += $VMSKU
-    }
+    $VMSKU = New-Object PSObject -Property @{
+        Name = $SKU.Name
+        Family = $SKU.Family -replace "standard", ""
+        SupportedEphemeralOSDiskPlacements = (($SKU.capabilities | Where-Object { $_.Name -eq "SupportedEphemeralOSDiskPlacements" }).Value)
+        CachedDiskGB = (($SKU.capabilities | Where-Object { $_.Name -eq "CachedDiskBytes" }).Value / 1GB)
+        MaxResourceVolumeGB = (($SKU.capabilities | Where-Object { $_.Name -eq "MaxResourceVolumeMB" }).Value / 1KB)
+        }
+    $VMCacheInfo += $VMSKU
 }
+
+$VMCacheInfo | Format-Table Name, SupportedEphemeralOSDiskPlacements, MaxResourceVolumeGB, CachedDiskGB -AutoSize
 
 
 $vmSizes=Get-AzComputeResourceSku | where{$_.ResourceType -eq 'virtualMachines' -and $_.Locations.Contains($location)}
