a33f7f20f1d7f4911401903ec6276afb97d42a86
John Savill <john@savilltech.com>
2025-02-26 06:31:37 -0800

    Update for the Private DNS Internet fallback whiteboard

--- PowerShellStuff/Restartmouseborderclipboard.ps1
+++ PowerShellStuff/Restartmouseborderclipboard.ps1
@@ -2,4 +2,3 @@ Write-Host "Killing PowerToys.MouseWithoutBordersHelper processes..."
 Stop-Process -Name "PowerToys.MouseWithoutBordersHelper*" -Force
 Write-Host "Starting PowerToys.MouseWithoutBordersHelper.exe..."
 Start-Process -FilePath "$env:USERPROFILE\AppData\Local\PowerToys\PowerToys.MouseWithoutBordersHelper.exe"
-https://www.linkedin.com/pulse/14th-february-2025-update-john-savill-zlyhc
